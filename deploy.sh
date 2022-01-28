ebcb7bac8d8058b255f178fd0ec5baf7b1a8223c
Giovani Camargo dos Anjos <giovanicamargodosanjos@gmail.com>
2022-01-28 11:21:48 -0300

    file deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-aws s3 cp source/parser.py s3://pismo-glue-scripts/parser/
-aws s3 cp source/distributor.py s3://pismo-glue-scripts/distributor/
+#!/bin/bash
+
+if python3 -m unittest source/test_functions.py
+then
+  aws s3 cp source/parser.py s3://pismo-glue-scripts/parser/
+  aws s3 cp source/distributor.py s3://pismo-glue-scripts/distributor/
+else
+  echo "Unit tests failed"
+fi
